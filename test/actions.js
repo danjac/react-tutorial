cdcc1f39d65cde5e57fbf509c5f69411d5f61fb5
danjac <danjac354@gmail.com>
2015-03-19 18:18:56 +0200

    testing actions

---
+++ test/actions.js
@@ -0,0 +1,20 @@
+import sinon from 'sinon';
+import {expect} from 'chai';
+import actions from '../client/actions';
+
+
+describe("login action", () => {
+
+    it('should work if valid identity and password', () => {
+
+    });
+
+    it('should call a failure if invalid identity and password', () => {
+
+    });
+
+    it('should call a failure if empty identity or password', () => {
+
+    });
+
+ });
