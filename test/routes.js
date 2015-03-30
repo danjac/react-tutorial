bf659f73f4c9de41060bd0f146356d00ecb9c6ee
danjac <danjac354@gmail.com>
2015-03-30 14:15:01 +0300

    Fixing server tests

--- test/routes.js
+++ test/routes.js
@@ -5,12 +5,20 @@ import bodyParser from 'body-parser';
 import errorHandler from 'errorhandler';
 import methodOverride from 'method-override';
 import expressJwt from 'express-jwt';
+import mongoose from 'mongoose';
+import ClearDB from 'mocha-mongoose';
 import {expect} from 'chai';
 
 import jsxRoutes from '../lib/frontend/Routes';
 import routes, {jwtToken} from '../lib/routes';
+import {Post, User} from '../lib/models';
 
 dotenv.load();
+
+const ObjectId = mongoose.Types.ObjectId;
+const dbUri = 'mongodb://localhost/react-tutorial-test';
+
+const clearDB = ClearDB(dbUri);
 
 const app = express();
 
@@ -33,24 +41,89 @@ app.use((req, res, next) => {
 
 routes(app);
 
-describe("DELETE /api/delete", () => {
+describe("DELETE /api/delete",function() {
+    this.timeout(6000);
+
+    beforeEach((done) => {
+        if (mongoose.connection.db) return done();
+        mongoose.connect(dbUri, done);
+    });
+    
+    beforeEach((done) => {
+        clearDB(done);
+    });
+
+    beforeEach((done) => {
+
+        new User({
+            name: 'testuser10',
+            email: 'teste10r@gmail.com',
+            password: 'testpass'
+        })
+        .save()
+        .then((user) => {
+            this.user = user;
+            return new Post({
+                title: 'testing something',
+                url: 'http://test',
+                author: this.user._id
+            }).save()
+        })
+        .then((post) => {
+            this.post = post;
+            done()
+        }, (err) => {
+            console.log(err)
+            done();
+        });
+    });
 
     it('should return a 401 if no user is authenticated', (done) => {
-        done();
+            request(app)
+                .delete('/api/' + this.post._id + '/')
+                .expect(401)
+                .end(done);
     });
 
     it('should return a 404 if no post exists', (done) => {
-        done();
+        request(app)
+            .delete('/api/' + ObjectId())
+            .set('authToken', this.user._id)
+            .expect(404)
+            .end(done);
     });
 
     it('should return a 404 if post does not belong to user', (done) => {
-
-        done();
+            new User({
+                name: 'testuser11',
+                email: 'test11@gmail.com',
+                password: 'testpass'
+            })
+            .save()
+            .then((user) => {
+                request(app)
+                .delete('/api/' + this.post._id + '/')
+                .set('authToken', user._id)
+                .expect(404)
+                .end(done)
+            });
     });
 
     it('should delete the post if the post belongs to the user', (done) => {
+        request(app)
+            .delete('/api/' + this.post._id + '/')
+            .set('authToken', this.user._id)
+            .expect(200)
+            .end(() => {
+                Post
+                    .count()
+                    .exec()
+                    .then((result) => {
+                        expect(result).to.equal(0);
+                        done();
+                    }, (err) => console.log(err))
+            });
 
-        done();
     });
 
 
